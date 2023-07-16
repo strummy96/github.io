858dd0436532ff1d189eabff0594eb7243281fc1
strummy96 <seth.strumwasser@gmail.com>
2023-07-15 21:36:59 -0400

    add images and data, try to load data

--- learn_your_birds/functions.js
+++ learn_your_birds/functions.js
@@ -1,7 +1,31 @@
+// import data
+const resp = await fetch('./data/species.json')
+const species = await resp.json()
+console.log(species)
+
 let open_to_settings = function(){
 	let x = document.getElementsByClassName("open_page")
-	console.log(x)
-	for (button in x){
-		button.visibility = false
+	for (let i = 0; i<3; i++){
+		x[i].style.display = 'none'
 		}
 }
+
+let unhide_buttons = function() {
+	let x = document.getElementsByClassName("open_page")
+	for (let i = 0; i<3; i++){
+		x[i].style.display = 'inline'
+		}
+}
+
+let multiple_choice = function() {
+	let x = document.getElementsByClassName('mc_settings')
+	for (let i=0; i<x.length + 1; i++) {
+		x[i].style.display = 'inline'
+}}
+
+// let setup = function() {
+	// import data from './data.species.json'
+// console.log(data)}
+	// for 
+		// let newCheck = document.createElement(input, {type: "checkbox"})
+		// let content = document.createTextNode(sp_name)
