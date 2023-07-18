62a90efbf569df42d17131d8e100b860b8ab825c
strummy96 <seth.strumwasser@gmail.com>
2023-07-17 21:35:51 -0400

    dynamic checkbox creation with borders on hover

--- learn_your_birds/functions.js
+++ learn_your_birds/functions.js
@@ -2,8 +2,16 @@
 async function get_data(){
 	console.log('Getting json data')
 	const resp = await fetch('https://raw.githubusercontent.com/strummy96/github.io/main/learn_your_birds/data/species.json')
-	const species = await resp.json()
-	console.log(species)
+	const s = await resp.json()
+	console.log(s.species)
+	for (let i=0; i<s.species.length; i++) {
+		console.log(s.species[i])
+		let newBox = document.createElement("label");
+		newBox.innerHTML = "<input class='mc_settings checkbox' type='checkbox'>" + s.species[i] + "</input>";
+		newBox.addEventListener("mouseover", function(){border_elem(newBox)}, true);
+		newBox.addEventListener("mouseout", function(){unborder_elem(newBox)}, true);
+		document.body.appendChild(newBox)
+	}
 }
 get_data()
 let open_to_settings = function(){
@@ -21,10 +29,9 @@ let unhide_buttons = function() {
 }
 
 let multiple_choice = function() {
-	let x = document.getElementsByClassName('mc_settings')
-	for (let i=0; i<x.length + 1; i++) {
-		x[i].style.display = 'inline'
-}}
+	let x = document.getElementById('species_checkboxes');
+	x.style.display = 'inline'
+}
 
 // let setup = function() {
 	// import data from './data.species.json'
@@ -32,3 +39,9 @@ let multiple_choice = function() {
 	// for 
 		// let newCheck = document.createElement(input, {type: "checkbox"})
 		// let content = document.createTextNode(sp_name)
+		
+function border_elem(x) {
+	x.style.border = "solid #000000"}
+	
+function unborder_elem(x) {
+	x.style.border = "none"}
