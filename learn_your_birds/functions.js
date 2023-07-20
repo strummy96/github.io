9ab4f85c336d333c2566296181d2284b781cf6d5
strummy96 <seth.strumwasser@gmail.com>
2023-07-19 21:29:38 -0400

    get list of selected spp to begin quiz

--- learn_your_birds/functions.js
+++ learn_your_birds/functions.js
@@ -1,30 +1,33 @@
 // import data
 async function get_data(){
-	console.log('Getting json data')
+	// pull in list of species
 	const resp = await fetch('https://raw.githubusercontent.com/strummy96/github.io/main/learn_your_birds/data/species.json')
 	const s = await resp.json()
 	console.log(s.species)
+	
+	// add a checkbox element for each species
 	let mc_div = document.getElementById('species_checkboxes');
 	mc_div.appendChild(document.createElement('br'))
 	for (let i=0; i<s.species.length; i++) {
-		console.log(s.species[i])
+		let newIn = document.createElement('input');
+		newIn.type = "checkbox";
+		newIn.id = s.species[i];
+		newIn.name = s.species[i];
+		newIn.className = "mc_settings sp_cbox"
 		let newBox = document.createElement("label");
-		newBox.innerHTML = "<input class='mc_settings checkbox' type='checkbox'>" + s.species[i] + "</input>";
+		newBox.innerHTML = s.species[i]
+		newBox.htmlFor = s.species[i]
 		newBox.addEventListener("mouseover", function(){border_elem(newBox)}, true);
 		newBox.addEventListener("mouseout", function(){unborder_elem(newBox)}, true);
 		newBox.style.display = "none";
 		newBox.className = "mc_settings";
-		mc_div.appendChild(newBox)
+		mc_div.appendChild(newIn);
+		mc_div.appendChild(newBox);
 		mc_div.appendChild(document.createElement('br'))
 	}
 }
+
 get_data()
-let open_to_settings = function(){
-	let x = document.getElementsByClassName("open_page")
-	for (let i = 0; i<3; i++){
-		x[i].style.display = 'none'
-		}
-}
 
 // things to do when 'Multiple Choice' button is clicked
 let multiple_choice = function() {
@@ -39,18 +42,9 @@ let multiple_choice = function() {
 	let but=document.getElementById('back_to_mm'); but.style.display="inline"
 }
 
-// let setup = function() {
-	// import data from './data.species.json'
-// console.log(data)}
-	// for 
-		// let newCheck = document.createElement(input, {type: "checkbox"})
-		// let content = document.createTextNode(sp_name)
-		
-function border_elem(x) {
-	x.style.border = "solid #000000"}
-	
-function unborder_elem(x) {
-	x.style.border = "none"}
+// functions for highligh on hover
+function border_elem(x) {x.style.border = "solid #000000"}
+function unborder_elem(x) {x.style.border = "none"}
 	
 function main_menu(){
 	let mm_elems = document.getElementsByClassName('open_page')
@@ -60,3 +54,15 @@ function main_menu(){
 		x[i].style.display = 'none'
 	}
 }
+
+function mc_quiz(){
+	// get list of species to include
+	let cboxes = document.getElementsByClassName('sp_cbox')
+	let spp = []
+	for (i=0;i<cboxes.length;i++) {
+		console.log(cboxes[i].checked); 
+		if(cboxes[i].checked){spp.push(cboxes[i].id)}
+	}
+	console.log(spp)
+}
+		
