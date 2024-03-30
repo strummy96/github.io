16f30ac4c1c950c7d5211adb7877b2651e41dae7
strummy96 <83839689+strummy96@users.noreply.github.com>
2024-03-30 14:44:35 -0700

    updates to afd page

--- weather4u/js/afd.js
+++ weather4u/js/afd.js
@@ -6,19 +6,15 @@ async function get_afd() {
     console.log(data);
 
     let last_afd = data["@graph"][0].id;
-    console.log(last_afd);
 
     let prod_url = "https://api.weather.gov/products/";
     let last_afd_url = prod_url + last_afd;
 
     const last_afd_resp = await fetch(last_afd_url);
     const last_afd_data = await last_afd_resp.json();
-    console.log(last_afd_data);
-    console.log(last_afd_data.productText);
 
     // Build AFD sections
     let afd_split = last_afd_data.productText.split("&&")
-    console.log(afd_split)
 
     for(section of afd_split){
         // remove credits section
@@ -26,18 +22,49 @@ async function get_afd() {
             continue
         }
         if(section.includes("NEAR TERM")){
+            let section_splitlines = section.split(/[\r\n]+/);
+            
+            let near_term_acc_button = document.querySelector("#near-term-acc-button");
+            for(line of section_splitlines){
+                if(line){near_term_acc_button.innerHTML = line; 
+                    section.replace(line, '');
+                    break}
+            }
+            
+
             let acc_near_term = document.querySelector("#acc-near-term");
             acc_near_term.innerHTML = "<p style='white-space: pre-line;'>" + 
                                         section + 
                                         "</p>";
         }
         if(section.includes("SHORT TERM")){
+            let section_splitlines = section.split(/[\r\n]+/);
+            
+            let near_term_acc_button = document.querySelector("#short-term-acc-button");
+            for(line of section_splitlines){
+                if(line){near_term_acc_button.innerHTML = line; 
+                    section.replace(line, '');
+                    break}
+            }
+            
             let acc_short_term = document.querySelector("#acc-short-term");
-            acc_short_term.innerHTML = section;
+            acc_short_term.innerHTML = "<p style='white-space: pre-line;'>" + 
+                                        section + 
+                                        "</p>";
         }
         if(section.includes("LONG TERM")){
+            let section_splitlines = section.split(/[\r\n]+/);
+            
+            let near_term_acc_button = document.querySelector("#long-term-acc-button");
+            for(line of section_splitlines){
+                if(line){near_term_acc_button.innerHTML = line; 
+                    section.replace(line, '');
+                    break}
+            }
             let acc_long_term = document.querySelector("#acc-long-term");
-            acc_long_term.innerHTML = section;
+            acc_long_term.innerHTML = "<p style='white-space: pre-line;'>" + 
+                                        section + 
+                                        "</p>";
         }
     }
 
