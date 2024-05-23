e50e060d13bf0355f9c075c8661af39d09b15ad4
strummy96 <83839689+strummy96@users.noreply.github.com>
2024-05-22 21:36:55 -0400

    afd - bold words first attempt

--- weather4u/js/afd.js
+++ weather4u/js/afd.js
@@ -1,5 +1,36 @@
 const afd_url = "https://api.weather.gov/products/types/AFD/locations/BOX"
 
+const b_words = [
+    "HOT",
+    "HEAT",
+    "WARM",
+    "WARMTH",
+    "MILD",
+    "COOL",
+    "COLD",
+
+    " RAIN ",
+    "RAINY",
+    " SHOWER ",
+    "SHOWERS",
+    "DRY",
+
+    "THUNDERSTORM ",
+    "THUNDERSTORMS",
+    "LIGHTNING",
+    "INSTABILITY",
+    "STORMS",
+
+    " STRONG ",
+    "SEVERE",
+    "UNSETTLED",
+
+    "WINDY",
+    "BREEZY",
+    "BREEZE",
+
+]
+
 async function get_afd() {
     const resp = await fetch(afd_url);
     const data = await resp.json();
@@ -21,51 +52,86 @@ async function get_afd() {
         if(section.includes("$$")){
             continue
         }
+
+        if(section.includes("SYNOPSIS")){
+            let section_splitlines = section.split(/[\r\n]+/);
+            
+            let syn_acc_button = document.querySelector("#synopsis-acc-button");
+            for(line of section_splitlines){
+                if(line.includes("SYNOPSIS")){syn_acc_button.innerHTML = line; 
+                    section.replace(line, '');
+                    break}
+            }
+            
+            let acc_synopsis = document.querySelector("#acc-synopsis");
+            acc_synopsis.innerHTML = "<p style='white-space: pre-line;'>" + 
+                                        bold_words(section) + 
+                                        "</p>";
+        }
+
         if(section.includes("NEAR TERM")){
             let section_splitlines = section.split(/[\r\n]+/);
             
             let near_term_acc_button = document.querySelector("#near-term-acc-button");
             for(line of section_splitlines){
-                if(line){near_term_acc_button.innerHTML = line; 
+                if(line != ""){
+                    near_term_acc_button.innerHTML = line; 
                     section.replace(line, '');
-                    break}
+                    break
+                }
             }
             
 
             let acc_near_term = document.querySelector("#acc-near-term");
             acc_near_term.innerHTML = "<p style='white-space: pre-line;'>" + 
-                                        section + 
+                                        bold_words(section) + 
                                         "</p>";
         }
+        
         if(section.includes("SHORT TERM")){
             let section_splitlines = section.split(/[\r\n]+/);
             
-            let near_term_acc_button = document.querySelector("#short-term-acc-button");
+            let short_term_acc_button = document.querySelector("#short-term-acc-button");
             for(line of section_splitlines){
-                if(line){near_term_acc_button.innerHTML = line; 
+                if(line != ""){short_term_acc_button.innerHTML = line; 
                     section.replace(line, '');
                     break}
             }
             
             let acc_short_term = document.querySelector("#acc-short-term");
             acc_short_term.innerHTML = "<p style='white-space: pre-line;'>" + 
-                                        section + 
+                                        bold_words(section) + 
                                         "</p>";
         }
         if(section.includes("LONG TERM")){
             let section_splitlines = section.split(/[\r\n]+/);
             
-            let near_term_acc_button = document.querySelector("#long-term-acc-button");
+            let long_term_acc_button = document.querySelector("#long-term-acc-button");
             for(line of section_splitlines){
-                if(line){near_term_acc_button.innerHTML = line; 
+                if(line != ""){long_term_acc_button.innerHTML = line; 
                     section.replace(line, '');
                     break}
             }
             let acc_long_term = document.querySelector("#acc-long-term");
             acc_long_term.innerHTML = "<p style='white-space: pre-line;'>" + 
-                                        section + 
+                                        bold_words(section) + 
                                         "</p>";
         }
     }
 
 }
+
+function bold_words(section) {
+    let b_section = section;
+    b_words.forEach((word) => {
+        const regex = new RegExp(word, "i");
+        b_section = b_section.replace(regex, "<strong>" + word.toLowerCase() + "</strong>");
+
+        // capitalize first letter if necessary
+        const cap_regex = new RegExp(/(?<=\. )([a-z])/, "g");
+        function replacer(match) { return match.toUpperCase() }
+        b_section = b_section.replace(cap_regex, replacer);
+    });
+    return b_section;
+}
+
